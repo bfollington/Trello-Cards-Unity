c7a6de96d4c7536ea9b179aa24c2531913b97e9c
Ben Follington <bfollington@gmail.com>
2014-12-12 20:45:29 +0700

    Added documentation and error handling for all basic functionality

---
+++ TrelloException.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+namespace Trello
+{
+	public class TrelloException : Exception {
+	
+		public TrelloException() : base()
+		{
+			
+		}
+		
+		public TrelloException(string message) : base(message)
+		{
+		
+		}
+		
+		public TrelloException(string format, params object[] args) : base(string.Format(format, args))
+		{
+		
+		}
+		
+		public TrelloException(string message, Exception innerException) : base(message, innerException) 
+		{
+		
+		}
+		
+		public TrelloException(string format, Exception innerException, params object[] args) : base(string.Format(format, args), innerException) 
+		{
+		
+		}
+	}
+}
